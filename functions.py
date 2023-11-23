64aec48d9447e80177a6304e05d20e6283ae5415
Kanyanjua Mwangi <kanyanjuamwangi@gmail.com>
2023-11-23 22:29:49 +0300

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,26 @@
+#def outputname():
+    #print(input("Name:"))
+
+#outputname()
+
+def monthly_expenses(rent, food):
+    total_expense = rent + food
+
+    return total_expense
+
+expenses = monthly_expenses(2000, 3000)
+
+#print(expenses)
+
+x = 10
+def addtwonum(z):
+    y = 2
+
+    print(y)
+    print(x)
+
+    sum = x +y +z
+    return sum
+
+print(addtwonum(10))
+
